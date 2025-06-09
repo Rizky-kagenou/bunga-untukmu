4450784b33111985d67215b3298161954dd13241
Rizky-kagenou <rizkyagungramadan269@gmail.com>
2025-06-09 19:04:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const rpc = document.getElementById("rose-petal-center");
+const rpc2 = document.getElementById("rose2-petal-center");
+const rpc3 = document.getElementById("rose3-petal-center");
+const rpl = document.getElementById("rose-petal-left");
+const rpl2 = document.getElementById("rose2-petal-left");
+const rpl3 = document.getElementById("rose3-petal-left");
+const rpr = document.getElementById("rose-petal-right");
+const rpr2 = document.getElementById("rose2-petal-right");
+const rpr3 = document.getElementById("rose3-petal-right");
+const rpil = document.getElementById("rose-petal-in-left");
+const rpil2 = document.getElementById("rose2-petal-in-left");
+const rpil3 = document.getElementById("rose3-petal-in-left");
+const rpir = document.getElementById("rose-petal-in-right");
+const rpir2 = document.getElementById("rose2-petal-in-right");
+const rpir3 = document.getElementById("rose3-petal-in-right");
+const rs = document.getElementById("rose-stem");
+const rs2 = document.getElementById("rose2-stem");
+const rs3 = document.getElementById("rose3-stem");
+const rl = document.getElementById("rose-leaf");
+const rll = document.getElementById("rose-leaf-2");
+const rl2 = document.getElementById("rose2-leaf");
+const rl3 = document.getElementById("rose3-leaf");
+
+function anim(){
+    let audio = new Audio("blue.mp3");
+    audio.play();
+    rpc.style.display = "flex";
+    rpc2.style.display = "flex";
+    rpc3.style.display = "flex";
+    rpl.style.display = "flex";
+    rpl2.style.display = "flex";
+    rpl3.style.display = "flex";
+    rpr.style.display = "flex";
+    rpr2.style.display = "flex";
+    rpr3.style.display = "flex";
+    rpil.style.display = "flex";
+    rpil2.style.display = "flex";
+    rpil3.style.display = "flex";
+    rpir.style.display = "flex";
+    rpir2.style.display = "flex";
+    rpir3.style.display = "flex";
+    rs.style.display = "flex";
+    rs2.style.display = "flex";
+    rs3.style.display = "flex";
+    rl.style.display = "flex";
+    rll.style.display = "flex";
+    rl2.style.display = "flex";
+    rl3.style.display = "flex";
+}
+
